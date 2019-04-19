70a8effc88c77d58b37ce3c783e3ae23d8b8b833
Igor Rud <rud.igor23@gmail.com>
2019-04-19 23:54:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+index_url = "./fixtures/student-site/index.html"
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+#name =
+#location =
+#profile_url =
