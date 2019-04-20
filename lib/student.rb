d9d1018738182ffe2263b6a04d5d0b86226745f9
Igor Rud <rud.igor23@gmail.com>
2019-04-20 00:35:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ students_array.each{|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
   end
 
   def self.all
