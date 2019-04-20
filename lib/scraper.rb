552fd7931317005353dd62edf2de939a8ba1885e
Igor Rud <rud.igor23@gmail.com>
2019-04-20 00:32:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,11 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
+
   def self.scrape_index_page(index_url)
-    html = open(index_url) #=> index_url = "../fixtures/student-site/index.html"
+    html = open(index_url)
     list = Nokogiri::HTML(html)
 
-    # This block returns a list of student names
     names = list.css(".student-name")
     names_array = []
     names.each do |item|
@@ -15,7 +15,6 @@ class Scraper
     end
     names_array
 
-    # This block returns a list of locations.
     locations = list.css(".student-location")
     location_array = []
     locations.each do |item|
@@ -23,7 +22,6 @@ class Scraper
     end
     location_array
 
-    # This block returns a list of student HTML pages.
     webpages = list.css(".student-card a[href]")
     webpage_array = []
     webpages.select do |item|
@@ -90,7 +88,6 @@ class Scraper
       end
     students_blog
 
-    # This needs to be an iteration where it starts with <<output = {}>>
     out_put = {:twitter=>students_twitter[0],
                :linkedin=>students_linkedin[0],
                :github=>students_github[0],
